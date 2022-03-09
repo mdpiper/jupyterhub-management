eb65d93460a1dc1a991c7b8875fb1caac86c09c6
Mark Piper <mark.piper@colorado.edu>
2022-03-09 10:34:36 -0700

    Simplify prompt

--- custom_prompt.sh
+++ custom_prompt.sh
@@ -2,4 +2,4 @@
 # Place this script in /etc/profile.d/ to set a custom prompt for all users.
 
 HUB=frontier
-export PROMPT_COMMAND='export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@${HUB}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "'
+export PROMPT_COMMAND='export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]${HUB}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "'
