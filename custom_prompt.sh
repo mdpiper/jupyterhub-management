f94e8ce9ca4b683c8d481dc6b9ddbd72de5bdf7d
Mark Piper <mark.piper@colorado.edu>
2022-07-25 14:49:04 -0600

    Update scripts for OES_HUB environment variable

--- custom_prompt.sh
+++ custom_prompt.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Place this script in /etc/profile.d/ to set a custom prompt for all users.
 
-HUB=frontier
-export PROMPT_COMMAND='export PS1="${CONDA_PROMPT_MODIFIER}${debian_chroot:+($debian_chroot)}\[\033[01;32m\]${HUB}\[\033[00m\] : \[\033[01;34m\]\w\[\033[00m\] \$ "'
+export PROMPT_COMMAND='export PS1="${CONDA_PROMPT_MODIFIER}${debian_chroot:+($debian_chroot)}\[\033[01;32m\]${OES_HUB}\[\033[00m\] : \[\033[01;34m\]\w\[\033[00m\] \$ "'
