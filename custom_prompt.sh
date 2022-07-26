27c1e481735795b390fea434f0565bc55737a120
Mark Piper <mark.piper@colorado.edu>
2022-07-26 11:34:40 -0600

    Set default value of OES_HUB

--- custom_prompt.sh
+++ custom_prompt.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Place this script in /etc/profile.d/ to set a custom prompt for all users.
 
+if [ -z $OES_HUB ]; then
+    OES_HUB="lab"
+fi
+
 export PROMPT_COMMAND='export PS1="${CONDA_PROMPT_MODIFIER}${debian_chroot:+($debian_chroot)}\[\033[01;32m\]${OES_HUB}\[\033[00m\] : \[\033[01;34m\]\w\[\033[00m\] \$ "'
