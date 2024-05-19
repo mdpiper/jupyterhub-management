98d827d59eb5ccc6ea82b4db3ae086df0915d066
Mark Piper <mark.piper@colorado.edu>
2024-05-18 18:23:24 -0600

    Working through `configure` dependencies
    Currently stuck on PDAL.

--- grass/install-grass.sh
+++ grass/install-grass.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Install GRASS GIS.
 
 GRASS_DIR=grass-src
 PREFIX=/usr/local
 
+# Install into a conda env: `bash install-grass.sh -p $CONDA_PREFIX``
 help() {
     echo -e "Usage: install-grass [-p] [-h]"
     exit 0
@@ -27,26 +29,28 @@ if [ ! -d $GRASS_DIR ]; then
     git clone --depth 1 --branch 8.3.1 https://github.com/OSGeo/grass.git $GRASS_DIR
 fi
 
-pushd $GRASS_DIR
-./configure \
+./$GRASS_DIR/configure \
     --prefix=$PREFIX \
+    --with-libs=$PREFIX/lib \
+    --with-includes=$PREFIX/include \
     --enable-largefile=yes \
     --with-nls \
     --with-cxx \
     --with-readline \
     --with-bzlib \
     --with-pthread \
-    --with-proj-share=/usr/share/proj \
-    --with-geos=/usr/bin/geos-config \
+    --with-proj \
+    --with-geos=$PREFIX/bin/geos-config \
     --with-cairo \
-    --with-opengl-libs=/usr/include/GL \
-    --with-freetype=yes --with-freetype-includes="/usr/include/freetype2/" \
-    --with-sqlite=yes \
+    --with-opengl \
+    --with-freetype \
+    --with-sqlite \
     --with-pdal \
-    --with-openmp
-make
-make install
+    --with-openmp \
+    --without-zstd
 
-popd
+if [ $? == 0 ]; then
+    make -C $GRASS_DIR install
+fi
 
-exit 0
+exit $?
